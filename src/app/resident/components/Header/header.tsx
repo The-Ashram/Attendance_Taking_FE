97b718982848245f8ae9ba942bacc04a047fa31b
EksdeeeX <Bryantanzhongkang@gmail.com>
2024-10-10 01:16:23 +0800

    Add request form for residents

--- src/app/resident/components/Header/header.tsx
+++ src/app/resident/components/Header/header.tsx
@@ -5,24 +5,41 @@ export default function Header() {
     const router = useRouter();
     const path = usePathname();
     const isHomepage = path === "/resident/homepage"
+    const isRequestForm = path === "/resident/requestform"
+
     function accountsHandler() {
         router.push('/resident/account')
     }
+    
     function homeHandler() {
         router.push('/resident/homepage')
     }
 
-    function logoutHandler() {
-        router.push('/')
+    function requestHandler() {
+        router.push('/resident/requestform')
     }
+
+    function logoutHandler() {
+        router.push('/')    
+    }
+
     return <Wrapper>
         {isHomepage ?
             <Title>Hello Resident</Title>
             :
             <Button onClick={homeHandler}>Home</Button>
         }
+
+        {isRequestForm ?
+            <Title >Reqeust form</Title>
+            :
+            <Button onClick={homeHandler}>Home</Button>
+
+        }
+        <Button onClick={requestHandler}>Submit Request Form</Button>
         <Button onClick={accountsHandler}>Account</Button>
         <Button onClick={logoutHandler}>Log Out</Button>
+        
     </Wrapper>
 
 }
