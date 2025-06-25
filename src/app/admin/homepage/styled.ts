48bcdb1c8c98125baf8fd608da4e6d549e76e8d7
APANA49 <aungphonenaing57@gmail.com>
2025-06-25 10:00:17 +0800

    Increased Date Time Font size

--- src/app/admin/homepage/styled.ts
+++ src/app/admin/homepage/styled.ts
@@ -1,24 +1,24 @@
 import styled from "styled-components";
 
 export const Date = styled.div`
-  font-size: 2vw; /* Base font size based on viewport width */
+  font-size: 4vw; /* Base font size based on viewport width */
   text-align: center;
-  margin-top: 2vh; /* Margin based on viewport height */
+  margin-top: 3vh; /* Margin based on viewport height */
   color: firebrick;
   font-weight: 700;
 
   @media (max-width: 1024px) {
-    font-size: 3vw; /* Slightly larger font size for tablets */
-    margin-top: 4vh; /* Adjust margin for tablets */
+    font-size: 4vw; /* Slightly larger font size for tablets */
+    margin-top: 6vh; /* Adjust margin for tablets */
   }
 
   @media (max-width: 768px) {
     font-size: 4vw; /* Increase font size for smaller screens */
-    margin-top: 5vh; /* Adjust margin for smaller screens */
+    margin-top: 4vh; /* Adjust margin for smaller screens */
   }
 
   @media (max-width: 480px) {
-    font-size: 5vw; /* Further increase font size for mobile */
-    margin-top: 6vh; /* Adjust margin for mobile screens */
+    font-size: 6vw; /* Further increase font size for mobile */
+    margin-top: 4vh; /* Adjust margin for mobile screens */
   }
 `;
